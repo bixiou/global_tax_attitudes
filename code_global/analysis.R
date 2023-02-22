c02b4d49f7262eb13878f56b9eb0811cf80537be
bixiou <fabre.adrien@hotmail.fr>
2023-02-22 20:36:34 +0100

    lité

--- code_global/analysis.R
+++ code_global/analysis.R
@@ -98,7 +98,7 @@ decrit("home_tenant", data = e)
 decrit("owner", data = e)
 decrit("wealth", data = e, miss = F)
 decrit("language", data = e, weight = F)
-CrossTable(e$language, e$country, prop.t = F, prop.r = F, prop.chisq = F, prop.c = F, total.c = F, total.r = F, cell.layout = F)
+CrossTable(e$language, e$country, prop.t = F, prop.r = F, prop.chisq = F, prop.c = F, total.c = F, total.r = F, cell.layout = F) # TODO! remove mismatch
 decrit("number_same_ip", data = e)
 representativeness_table(c("US1"), return_table = F)
 
