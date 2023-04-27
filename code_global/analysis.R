28947a9c349a9ca48b14169a04f6c93b71b36eee
bixiou <fabre.adrien@hotmail.fr>
2023-04-27 16:47:04 +0200

    CI support

--- code_global/analysis.R
+++ code_global/analysis.R
@@ -406,3 +406,13 @@ decrit("gcs_support", eu)
 decrit("gcs_support", eu, weights = eu$weight_vote)
 decrit("gcs_support", us1)
 decrit("gcs_support", us1, weights = us1$weight_vote)
+
+
+##### CI support ####
+binconf(sum(us1$weight[us1$gcs_support == 'Yes']), nrow(us1))
+binconf(sum(us2$weight[us2$gcs_support == 'Yes' & us2$branch_gcs == 'important']), sum(us2$weight[us2$branch_gcs == 'important']))
+binconf(sum(us2$weight[us2$gcs_support == 'Yes' & us2$branch_gcs == 'field']), sum(us2$weight[us2$branch_gcs == 'field']))
+binconf(sum(us2$weight[us2$gcs_support == 'Yes' & us2$branch_gcs %in% c('field', 'important')]), sum(us2$weight[us2$branch_gcs %in% c('field', 'important')]))
+us3 <- us2[us2$branch_gcs == 'important', ]
+reweighted_estimate("gcs_support", "US3", verbose = T)
+rm(us3)
